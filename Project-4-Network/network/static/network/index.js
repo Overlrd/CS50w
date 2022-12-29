c8017d3f097b870f429e36c98a6f874b216823ab
Overlrd <premieraugust28@gmail.com>
2022-12-29 11:53:33 +0000

    styling add post all posts

--- Project-4-Network/network/static/network/index.js
+++ Project-4-Network/network/static/network/index.js
@@ -32,12 +32,51 @@ function load_post(which){
     
                 post_container = document.createElement('div');
                 post_container.id = `post-container-${post_id}`
-                post_container.className += "post_container"
+                post_container.className += "tweet_container"
     
-                post_container.innerHTML = `<div style='margin:10px; font-weight:bold;'>
-                From : <a id='username' href='#'> ${post_user} <a/> </div> <div style='margin:10px;'>Says : ${post_content}</div> 
-                <div style='margin:10px; font-weight:bold;'>Date : ${post_timestamp}</div>
-                <div style='margin:10px; font-weight:bold;'>Likes : ${post_num_like}</div>`
+                post_container.innerHTML = `
+                <div class="image_container">
+                <img  src="https://i.imgur.com/0PmI3ZD.png" alt="" srcset="">                    
+                </div>
+
+
+                <div class="tweet_body_container" >
+                    <div class="tweet_header_container">
+                        <span class="tweet_username" >${post_user}</span>  <span class="tweet_user_at" >SudoOverloord</span> <span class="tweet_date">${post_timestamp}</span>
+
+                    </div>
+                    <div class="tweet_body_text">
+                        <p id="tweet_body_text">
+                            ${post_content}
+                        </p>
+                    </div>
+                    <div class="tweet_footer">
+                        <div class="tweet_footer_item">
+                            <span>
+                                <i class="fa-regular fa-heart"></i>
+                            </span>
+                            <p>${post_num_like}</p>
+                        </div>
+
+                        <div class="tweet_footer_item">
+                            <span>
+                                <i class="fa-regular fa-heart"></i>
+                            </span>
+                            <p>15</p>
+                        </div>
+
+                        <div class="tweet_footer_item">
+                            <span>
+                                <i class="fa-regular fa-heart"></i>
+                            </span>
+                            <p>15</p>
+                        </div>
+
+
+                    </div>
+                </div>
+
+                `
 
 
                 document.querySelector('#all-posts-view').appendChild(post_container)    
